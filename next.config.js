da99308b2e989ff2f6afaba4ebb1e8bc1314d3c7
priyam <pshah@havenhealthmgmt.org>
2024-12-26 16:37:47 -0500

    fix app deployment process atmpt 1

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'standalone',  // This is important for deployment
+  }
+  
+  module.exports = nextConfig
